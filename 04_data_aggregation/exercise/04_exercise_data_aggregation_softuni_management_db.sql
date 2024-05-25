c3780c46f8348b13edccabb47369536818835c71
kiril <kiriltanev@proton.me>
2024-05-25 12:47:25 +0300

    04_exercise part 1 solutions updated

---
+++ 04_data_aggregation/exercise/04_exercise_data_aggregation_softuni_management_db.sql
@@ -0,0 +1,15 @@
+-- 13. SUM the Employees
+
+-- 14. Update Employees’ Data
+
+-- 15. Categorizes Salary
+
+-- 16. WHERE Project Status
+
+-- 17. HAVING Salary Level
+
+-- 18. Nested CASE Conditions
+
+-- 19. Foreign Key
+
+-- 20. JOIN Tables
