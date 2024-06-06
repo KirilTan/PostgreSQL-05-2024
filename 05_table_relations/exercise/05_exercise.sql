de5cd57d0470e104eecb71fa55bad9d3a9a8e82a
kiril <kiriltanev@proton.me>
2024-06-06 11:54:04 +0300

    05_exercise part 1 solutions added

--- 05_table_relations/exercise/05_exercise.sql
+++ 05_table_relations/exercise/05_exercise.sql
@@ -148,8 +148,143 @@ VALUES
 	('philip_1123.com', 'Roman Theater', 2);
 
 -- 08. Study Session
+CREATE TABLE IF NOT EXISTS
+    students
+(
+    id           INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    student_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+    exams
+(
+    id        INT GENERATED ALWAYS AS IDENTITY (START 101 INCREMENT 1) PRIMARY KEY,
+    exam_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+    study_halls
+(
+    id              INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    study_hall_name VARCHAR(50),
+    exam_id         INT,
+
+    CONSTRAINT fk_study_halls_exams
+        FOREIGN KEY (exam_id)
+            REFERENCES exams (id)
+);
+
+CREATE TABLE IF NOT EXISTS
+    students_exams
+(
+    student_id INT,
+    exam_id    INT,
+
+    CONSTRAINT pk_students_exams
+        PRIMARY KEY (student_id, exam_id),
+
+    CONSTRAINT fk_students_exams_students
+        FOREIGN KEY (student_id)
+            REFERENCES students (id),
+
+    CONSTRAINT fk_students_exams_exams
+        FOREIGN KEY (exam_id)
+            REFERENCES exams (id)
+);
+
+INSERT INTO students(student_name)
+VALUES ('Mila'),
+       ('Toni'),
+       ('Ron');
+
+INSERT INTO exams(exam_name)
+VALUES ('Python Advanced'),
+       ('Python OOP'),
+       ('PostgreSQL');
+
+INSERT INTO study_halls(study_hall_name, exam_id)
+VALUES ('Open Source Hall', 102),
+       ('Inspiration Hall', 101),
+       ('Creative Hall', 103),
+       ('Masterclass Hall', 103),
+       ('Information Security Hall', 103);
+
+INSERT INTO students_exams
+VALUES (1, 101),
+       (1, 102),
+       (2, 101),
+       (3, 103),
+       (2, 102),
+       (2, 103);
 
 -- 10. Online Store
+CREATE TABLE IF NOT EXISTS
+    item_types
+(
+    id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    item_type_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+    items
+(
+    id           INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    item_name    VARCHAR(50),
+    item_type_id INT,
+
+    CONSTRAINT fk_items_item_types
+        FOREIGN KEY (item_type_id)
+            REFERENCES item_types (id)
+);
+
+CREATE TABLE IF NOT EXISTS
+    cities
+(
+    id        INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    city_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+    customers
+(
+    id            INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    customer_name VARCHAR(50),
+    birthday      DATE,
+    city_id       INT,
+
+    CONSTRAINT fk_customers_cities
+        FOREIGN KEY (city_id)
+            REFERENCES cities (id)
+);
+
+CREATE TABLE IF NOT EXISTS
+    orders
+(
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    customer_id INT,
+
+    CONSTRAINT fk_customer_id
+        FOREIGN KEY (customer_id)
+            REFERENCES customers (id)
+);
+
+CREATE TABLE IF NOT EXISTS
+    order_items
+(
+    order_id INT,
+    item_id  INT,
+
+    CONSTRAINT pk_order_items
+        PRIMARY KEY (order_id, item_id),
+
+    CONSTRAINT fk_order_items_orders
+        FOREIGN KEY (order_id)
+            REFERENCES orders (id),
+
+    CONSTRAINT fk_order_items_items
+        FOREIGN KEY (item_id)
+            REFERENCES items (id)
+);
 
 -- 11. Delete Cascade
 
