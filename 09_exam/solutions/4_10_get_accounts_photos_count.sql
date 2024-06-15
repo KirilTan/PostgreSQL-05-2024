3eb21169d0577b9da56587b495350887923f1766
kiril <kiriltanev@proton.me>
2024-06-15 10:13:12 +0300

    4_10 solution added

---
+++ 09_exam/solutions/4_10_get_accounts_photos_count.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION udf_accounts_photos_count(account_username VARCHAR(30))
+    RETURNS INT AS
+$$
+BEGIN
+    RETURN (SELECT COUNT(*)
+            FROM accounts AS a
+                     JOIN accounts_photos AS ap
+                          ON ap.account_id = a.id
+            WHERE a.username = account_username);
+END;
+$$
+    LANGUAGE plpgsql;
